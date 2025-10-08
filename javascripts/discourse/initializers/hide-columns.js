1b86055b94ff29755da4708e2000f080d30c7817
Jarek Radosz <jarek@cvx.dev>
2025-10-08 23:32:50 +0200

    DEV: Update linting

--- javascripts/discourse/initializers/hide-columns.js
+++ javascripts/discourse/initializers/hide-columns.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("0.8.0", (api) => {
+export default apiInitializer((api) => {
   api.registerValueTransformer("topic-list-columns", ({ value: columns }) => {
     columns.delete("views");
     columns.delete("posters");
